18a25357df2c1a0dc20838739f2631ac3dc5f01e
EricBryle <135036494+EricBryle@users.noreply.github.com>
2023-06-02 13:14:17 +0800

    Add files via upload

---
+++ ClinicDashBoard/src/main/java/com/mycompany/clinicdashboard/ClinicDashBoard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicdashboard;
+
+/**
+ *
+ * @author ericb
+ */
+public class ClinicDashBoard {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
