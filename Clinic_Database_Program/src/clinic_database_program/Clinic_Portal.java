03df1b26e083ccb1d3fb7442ea0847f81bcb1cb5
Jyskiii <jyrohgomez@gmail.com>
2023-06-04 07:18:23 +0800

    New Folder
    New

--- Clinic_Database_Program/src/clinic_database_program/Clinic_Portal.java
+++ Clinic_Database_Program/src/clinic_database_program/Clinic_Portal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package clinic_database_program;
+package clinic_program;
 
 import javax.swing.JOptionPane;
 
